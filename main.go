f1fe4bfb2111199d5004d6c7062b3d309a25bd3b
Max <maxabcr2000@gmail.com>
2018-03-23 10:36:50 +0800

    Example code for adding a bunch of data with a wrapper folder programmtically to IPFS

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/ipfs/go-ipfs-api"
+	// files "github.com/ipfs/go-ipfs-cmdkit/files"
+)
+
+const (
+	url = "localhost:5001"
+)
+
+var (
+	MAGIC_BYTES = fmt.Sprintf("%c%c", 8, 1)
+)
+
+func main() {
+	sh := shell.NewShell(url)
+	hash1, err := sh.AddNoPin(bytes.NewBufferString("Hello IPFS Shell tests"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hash1:", hash1)
+
+	hash2, err := sh.AddNoPin(bytes.NewBufferString("I want to test hash."))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hash2:", hash2)
+
+	hash3, err := sh.AddNoPin(bytes.NewBufferString("I want to go work out."))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hash3:", hash3)
+
+	rootObj := &shell.IpfsObject{
+		Links: []shell.ObjectLink{},
+		Data:  MAGIC_BYTES,
+	}
+
+	root, err := sh.ObjectPut(rootObj)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("root before:", root)
+
+	root1, err := sh.PatchLink(root, "hash1", hash1, false)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("root1:", root1)
+
+	root2, err := sh.PatchLink(root1, "hash2", hash2, false)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("root2:", root2)
+
+	root3, err := sh.PatchLink(root2, "hash3", hash3, false)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("root3:", root3)
+
+	dir, _ := sh.ObjectGet("QmeYye6wzMCAG67vQtYn1pAF3p89pWnfBx2GcLMeJ6XNBP")
+
+	dirList, _ := sh.List("/ipfs/QmeYye6wzMCAG67vQtYn1pAF3p89pWnfBx2GcLMeJ6XNBP")
+	fmt.Println("dirList:", dirList)
+
+	fmt.Println("dir.Data:", dir.Data)
+	fmt.Println("rootObj.Data:", rootObj.Data)
+
+	rootList, _ := sh.List(fmt.Sprintf("/ipfs/%s", root3))
+	fmt.Println("rootList:", rootList)
+}
